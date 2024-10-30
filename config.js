6e4c5b92f77eed5c6a7a0f2dce64faf80bdeef0e
crush11b <72981240+crush11b@users.noreply.github.com>
2024-10-30 18:30:01 -0400

    Update config.js

--- config.js
+++ config.js
@@ -18,23 +18,24 @@ const aURL = [
   ["2196F3", "LIGHTNING", "https://map.blitzortung.org/#6.89/37.412/-78.706", "1", "R"],
   ["2196F3", "PISTAR", "http://pi-star.local/", "1.2"],
   ["2196F3", "TIME.IS", "https://time.is/", "1", "R"],
+   ["2196F3", "Outlook", "https://www.weather.gov/media/akq/briefings/LatestBriefing.pdf", "1", "R"]
   ["2196F3", "RADAR", "https://radar.weather.gov/?settings=v1_eyJhZ2VuZGEiOnsiaWQiOm51bGwsImNlbnRlciI6Wy05Ni42ODMsMzguMDIxXSwibG9jYXRpb24iOm51bGwsInpvb20iOjUuNDE5MTY1MzQ1MTU0NDQ3fSwiYW5pbWF0aW5nIjpmYWxzZSwiYmFzZSI6InN0YW5kYXJkIiwiYXJ0Y2MiOmZhbHNlLCJjb3VudHkiOmZhbHNlLCJjd2EiOmZhbHNlLCJyZmMiOmZhbHNlLCJzdGF0ZSI6ZmFsc2UsIm1lbnUiOnRydWUsInNob3J0RnVzZWRPbmx5IjpmYWxzZSwib3BhY2l0eSI6eyJhbGVydHMiOjAuOCwibG9jYWwiOjAuNiwibG9jYWxTdGF0aW9ucyI6MC44LCJuYXRpb25hbCI6MC42fX0%3D", "1", "R"],
   ["2196F3", "WEATHER", "https://ambientweather.net/dashboard/bea6b10a09ad231317312d829b55f271", "1", "R"],
   ["2196F3", "WINDS", "https://www.ventusky.com/?p=37.84;-78.63;7&l=wind-10m", "1", "R"],
-  ["2196F3", "Outlook", "https://www.weather.gov/media/akq/briefings/LatestBriefing.pdf", "1", "R"]
+ 
 ];
 
 // Dashboard items
 // Structure: [Title, Image Source URL]
 const aIMG = [
-  ["RADAR", "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"],
-  ["LOCAL RADAR", "https://radar.weather.gov/ridge/standard/KAKQ_loop.gif"],
-  ["Satellite", "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/CONUS/EXTENT3/GOES16-CONUS-EXTENT3-625x375.gif"],
-  ["Tropical Outlook", "https://www.nhc.noaa.gov/xgtwo/two_atl_0d0.png", "https://www.nhc.noaa.gov/xgtwo/two_atl_2d0.png", "https://www.nhc.noaa.gov/xgtwo/two_atl_7d0.png"],
-  ["", "http://192.168.0.104:8081/live.html"],
+  ["", "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"],
+  ["", "https://radar.weather.gov/ridge/standard/KAKQ_loop.gif"],
+  ["", "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/CONUS/EXTENT3/GOES16-CONUS-EXTENT3-625x375.gif"],
+  ["", "https://www.nhc.noaa.gov/xgtwo/two_atl_0d0.png", "https://www.nhc.noaa.gov/xgtwo/two_atl_2d0.png", "https://www.nhc.noaa.gov/xgtwo/two_atl_7d0.png"],
+  ["", "https://forecast.weather.gov/MapClick.php?lat=37.4006&lon=-77.4531l"],
+  ["", "https://forecast.weather.gov/MapClick.php?lat=37.4006&lon=-77.4531l"],
   ["SATELLITE CGL", "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/cgl/EXTENT3/GOES16-CGL-EXTENT3-600x600.gif"],
   ["LIGHTNING", "https://images.lightningmaps.org/blitzortung/america/index.php?animation=usa"],
-  ["LIGHTNING LOCAL", "https://www.blitzortung.org/en/Images/image_b_ny.png"],
   ["YOUTUBE EXAMPLE", "iframe|https://www.youtube.com/embed/fzPFaXAV_2Y?autoplay=1&mute=1"],
   ["WEBSITE EXAMPLE", "iframe|https://globe.adsbexchange.com/?airport=YYZ"],
   ["VIDEO EXAMPLE", "https://himawari8.nict.go.jp/movie/720/20240611_pifd.mp4"],
